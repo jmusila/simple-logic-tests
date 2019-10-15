49705a428effbf42971b263bfa0b523ee025fbca
jonathanmusila <jonathanmusila6@gmail.com>
2019-10-15 21:33:09 +0300

    added method reverse

---
+++ palindrome/is_palindrome.py
@@ -0,0 +1,19 @@
+# this code will accept an input string and check if it is a plandrome
+# it will then return true if it is a plaindrome and false if it is not
+
+
+def reverse(str1):
+    if(len(str1) == 0):
+        return str1
+    else:
+        return reverse(str1[1:]) + str1[0]
+
+
+string = input("Please enter your own String : ")
+str1 = reverse(string)
+print("String in reverse Order :  ", str1)
+
+if(string == str1):
+    print("This is a Palindrome String")
+else:
+    print("This is Not a Palindrome String")
