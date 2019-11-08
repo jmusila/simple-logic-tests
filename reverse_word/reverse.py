edd21e317f8d0e5dfd359a6eb4177c9365f4ecc0
jonathanmusila <jonathanmusila6@gmail.com>
2019-11-08 15:08:06 +0300

    added reverse words

---
+++ reverse_word/reverse.py
@@ -0,0 +1,9 @@
+# This file contains a function to reverse word,
+# For example, given the word Jonathan,
+# Then it shall be reversed to nahtanoJ
+
+def revrseWords(input):
+
+    # split the words in the string separated by space
+    inputWords = input.split(" ")
+    
