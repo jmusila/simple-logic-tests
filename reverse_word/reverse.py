adaa3e112dfb89958ac36b58455d8d90bbea3655
jonathanmusila <jonathanmusila6@gmail.com>
2019-11-08 15:16:02 +0300

    added the print statement

--- reverse_word/reverse.py
+++ reverse_word/reverse.py
@@ -2,11 +2,18 @@
 # For example, given the word Jonathan,
 # Then it shall be reversed to nahtanoJ
 
-def revrseWords(input):
+def reverseWords(input):
 
     # split the words in the string separated by space
     inputWords = input.split(" ")
 
     inputWords = inputWords[-1::-1]
 
-    
+    # join the spaced words
+    reversed_words  = " ".join(inputWords)
+
+    return reversed_words
+
+if __name__ == "__main__":
+    input = "I am really becoming good at this"
+    print(reverseWords(input))
